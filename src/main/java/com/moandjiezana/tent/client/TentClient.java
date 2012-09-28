b504d1262dd9b350771718129738d8bdbc604ebf
moandji.ezana <mwanji@gmail.com>
2012-09-28 19:53:47 +0200

    Created TentClientAsync and made TentClient a synchronous wrapper around it.

--- src/main/java/com/moandjiezana/tent/client/TentClient.java
+++ src/main/java/com/moandjiezana/tent/client/TentClient.java
@@ -1,48 +1,26 @@
 package com.moandjiezana.tent.client;
 
 import com.moandjiezana.tent.client.apps.RegistrationRequest;
+import com.moandjiezana.tent.client.apps.RegistrationResponse;
 import com.moandjiezana.tent.client.users.Following;
 import com.moandjiezana.tent.client.users.Profile;
-import com.ning.http.client.AsyncHttpClient;
-import com.ning.http.client.Response;
 
-import java.io.StringWriter;
-import java.util.ArrayList;
 import java.util.List;
 
-import org.codehaus.jackson.JsonNode;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.type.TypeReference;
-
 /**
- * As this class is stateful, each instance should only be used for one entity at a time.
+ * A synchronous wrapper around {@link TentClientAsync}
  */
 public class TentClient {
-  
-  private static final String TENT_MIME_TYPE = "application/vnd.tent.v0+json";
-  private final AsyncHttpClient httpClient = new AsyncHttpClient();
-  private final ObjectMapper objectMapper = new ObjectMapper();
-
-  private String[] servers;
-  private List<String> profileUrls;
+  private final TentClientAsync tentClientAsync = new TentClientAsync();
   
   /**
    * Use the default constructor only to discover an entity.
    */
   public TentClient() {}
   
-  /**
-   * Obtains the profile URLs and API roots for the given entity.
-   * All future method calls use these URLs.
-   * @param entityUrl
-   */
-  public void discover(String entityUrl) {
+  public List<String> discover(String entityUrl) {
     try {
-      Response response = httpClient.prepareHead(entityUrl).addHeader("Accept", TENT_MIME_TYPE).execute().get();
-      addProfileUrls(response);
-      
-      Profile profile = getProfile();
-      addServers(profile.getCore());
+      return tentClientAsync.discover(entityUrl).get();
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
@@ -50,22 +28,7 @@ public class TentClient {
   
   public Profile getProfile() {
     try {
-      Response response = httpClient.prepareGet(profileUrls.get(0)).addHeader("Accept", TENT_MIME_TYPE).execute().get();
-      
-      String responseBody = response.getResponseBody();
-      JsonNode json = objectMapper.readValue(responseBody, JsonNode.class);
-      
-      Profile profile = new Profile();
-      
-      if (json.has(Profile.Core.URI)) {
-        profile.setCore(objectMapper.readValue(json.get(Profile.Core.URI), Profile.Core.class));
-      }
-      
-      if (json.has(Profile.Basic.URI)) {
-        profile.setBasic(objectMapper.readValue(json.get(Profile.Basic.URI), Profile.Basic.class));
-      }
-      
-      return profile;
+      return tentClientAsync.getProfile().get();
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
@@ -73,42 +36,17 @@ public class TentClient {
   
   public List<Following> getFollowings() {
     try {
-      Response response = httpClient.prepareGet(servers[0] + "/followings").addHeader("Accept", TENT_MIME_TYPE).execute().get();
-      return objectMapper.readValue(response.getResponseBody(), new TypeReference<List<Following>>() {});
+      return tentClientAsync.getFollowings().get();
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
   }
   
-  public void register(RegistrationRequest registrationRequest) {
-    StringWriter jsonWriter = new StringWriter();
+  public RegistrationResponse register(RegistrationRequest registrationRequest) {
     try {
-      new ObjectMapper().writeValue(jsonWriter, registrationRequest);
-      
-      httpClient.preparePost(servers[0] + "/apps")
-        .addHeader("Content-Type", TENT_MIME_TYPE)
-        .addHeader("Accept", TENT_MIME_TYPE)
-        .setBody(jsonWriter.toString())
-        .execute();
+      return tentClientAsync.register(registrationRequest).get();
     } catch (Exception e) {
       throw new RuntimeException(e);
-    }
-  }
-  
-  private void addServers(Profile.Core core) {
-    servers = core.getServers();
-  }
-  
-  private void addProfileUrls(Response response) {
-    profileUrls = new ArrayList<String>();
-    String header = response.getHeader("Link");
-    String[] rawLinks = header.split(",");
-    
-    for (String rawLink : rawLinks) {
-      String[] urlAndRel = rawLink.split(";");
-      if (urlAndRel.length == 2 && urlAndRel[1].equals(" rel=\"https://tent.io/rels/profile\"")) {
-        profileUrls.add(urlAndRel[0].substring(1, urlAndRel[0].length() - 1));
-      }
     }
   }
 }
---
+++ src/main/java/com/moandjiezana/tent/client/TentClientAsync.java
@@ -0,0 +1,145 @@
+package com.moandjiezana.tent.client;
+
+import com.moandjiezana.tent.client.apps.RegistrationRequest;
+import com.moandjiezana.tent.client.apps.RegistrationResponse;
+import com.moandjiezana.tent.client.users.Following;
+import com.moandjiezana.tent.client.users.Profile;
+import com.moandjiezana.tent.http.SimpleAsyncHandler;
+import com.ning.http.client.AsyncHttpClient;
+import com.ning.http.client.Response;
+
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Future;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
+
+/**
+ * As this class is stateful, each instance should only be used for one entity at a time.
+ */
+public class TentClientAsync {
+  
+  private static final String TENT_MIME_TYPE = "application/vnd.tent.v0+json";
+  private final AsyncHttpClient httpClient = new AsyncHttpClient();
+  private final ObjectMapper objectMapper = new ObjectMapper();
+
+  private String[] servers;
+  private List<String> profileUrls;
+  
+  /**
+   * Use the default constructor only to discover an entity.
+   */
+  public TentClientAsync() {}
+  
+  /**
+   * Obtains the profile URLs for the given entity.
+   * All future method calls use these URLs.
+   * @param entityUrl
+   * @return 
+   */
+  public Future<List<String>> discover(String entityUrl) {
+    try {
+      return httpClient.prepareHead(entityUrl).addHeader("Accept", TENT_MIME_TYPE).execute(new SimpleAsyncHandler<List<String>>() {
+        @Override
+        protected List<String> doOnCompleted(Response response) throws Exception {
+          addProfileUrls(response);
+          servers = null;
+          
+          return profileUrls;
+        }
+      });
+      
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+  
+  /**
+   * Sets the API roots if necessary.
+   * @return
+   */
+  public Future<Profile> getProfile() {
+    try {
+      return httpClient.prepareGet(profileUrls.get(0)).addHeader("Accept", TENT_MIME_TYPE).execute(new SimpleAsyncHandler<Profile>() {
+        @Override
+        protected Profile doOnCompleted(Response response) throws Exception {
+          String responseBody = response.getResponseBody();
+          JsonNode json = objectMapper.readValue(responseBody, JsonNode.class);
+          
+          Profile profile = new Profile();
+          
+          if (json.has(Profile.Core.URI)) {
+            profile.setCore(objectMapper.readValue(json.get(Profile.Core.URI), Profile.Core.class));
+            
+            if (servers == null) {
+              addServers(profile.getCore());
+            }
+          }
+          
+          if (json.has(Profile.Basic.URI)) {
+            profile.setBasic(objectMapper.readValue(json.get(Profile.Basic.URI), Profile.Basic.class));
+          }
+          
+          return profile;
+        }
+      });
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+  
+  public Future<List<Following>> getFollowings() {
+    try {
+      return httpClient.prepareGet(servers[0] + "/followings").addHeader("Accept", TENT_MIME_TYPE).execute(new SimpleAsyncHandler<List<Following>>() {
+        @Override
+        protected List<Following> doOnCompleted(Response response) throws Exception {
+          return objectMapper.readValue(response.getResponseBody(), new TypeReference<List<Following>>() {});
+        }
+      });
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+  
+  public Future<RegistrationResponse> register(RegistrationRequest registrationRequest) {
+    StringWriter jsonWriter = new StringWriter();
+    try {
+      new ObjectMapper().writeValue(jsonWriter, registrationRequest);
+      
+      return httpClient.preparePost(servers[0] + "/apps")
+        .addHeader("Content-Type", TENT_MIME_TYPE)
+        .addHeader("Accept", TENT_MIME_TYPE)
+        .setBody(jsonWriter.toString())
+        .execute(new SimpleAsyncHandler<RegistrationResponse>() {
+          @Override
+          protected RegistrationResponse doOnCompleted(Response response) throws Exception {
+            String responseBody = response.getResponseBody();
+            System.out.println(responseBody);
+            return objectMapper.readValue(responseBody, RegistrationResponse.class);
+          }
+        });
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+  
+  private void addServers(Profile.Core core) {
+    servers = core.getServers();
+  }
+  
+  private void addProfileUrls(Response response) {
+    profileUrls = new ArrayList<String>();
+    String header = response.getHeader("Link");
+    String[] rawLinks = header.split(",");
+    
+    for (String rawLink : rawLinks) {
+      String[] urlAndRel = rawLink.split(";");
+      if (urlAndRel.length == 2 && urlAndRel[1].equals(" rel=\"https://tent.io/rels/profile\"")) {
+        profileUrls.add(urlAndRel[0].substring(1, urlAndRel[0].length() - 1));
+      }
+    }
+  }
+}
