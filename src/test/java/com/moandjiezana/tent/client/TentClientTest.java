b0fa86684c841d02c6ead6a531b38c4c16033662
moandji.ezana <mwanji@gmail.com>
2012-09-30 00:49:48 +0200

    Test discovery via response header

--- src/test/java/com/moandjiezana/tent/client/TentClientTest.java
+++ src/test/java/com/moandjiezana/tent/client/TentClientTest.java
@@ -1,14 +1,20 @@
 package com.moandjiezana.tent.client;
 
+import static com.github.restdriver.clientdriver.RestClientDriver.giveEmptyResponse;
+import static com.github.restdriver.clientdriver.RestClientDriver.giveResponse;
+import static com.github.restdriver.clientdriver.RestClientDriver.onRequestTo;
+import static org.junit.Assert.assertEquals;
+
+import com.github.restdriver.clientdriver.ClientDriverRequest.Method;
+import com.github.restdriver.clientdriver.ClientDriverRule;
 import com.moandjiezana.tent.client.posts.Mention;
 import com.moandjiezana.tent.client.posts.Post;
 import com.moandjiezana.tent.client.posts.Status;
-import com.moandjiezana.tent.client.users.Following;
 import com.moandjiezana.tent.client.users.Profile;
 
-import java.util.HashMap;
 import java.util.List;
 
+import org.junit.Rule;
 import org.junit.Test;
 
 
@@ -16,36 +22,40 @@ import org.junit.Test;
  * Unit test for simple App.
  */
 public class TentClientTest {
+  
+  @Rule
+  public final ClientDriverRule server = new ClientDriverRule();
+  private final TentClientAsync tentClient = new TentClientAsync();
 
   @Test
   public void explore() {
-    TentClient tentClient = new TentClient();
-    tentClient.discover("https://mwanji.tent.is");
-    Profile profile = tentClient.getProfile();
-    
-    System.out.println(profile.getCore().getEntity());
-    System.out.println(profile.getBasic().getName());
-    System.out.println(profile.getBasic().getAvatarUrl());
-    
-    List<Post> posts = tentClient.getPosts();
-    
-    printPosts(posts);
-    
-    List<Following> followings = tentClient.getFollowings();
-    
-    for (Following following : followings) {
-      TentClient followingTentClient = new TentClient();
-      followingTentClient.discover(following.getEntity());
-      Profile followingProfile = followingTentClient.getProfile();
-      Following followingDetail = tentClient.getFollowing(following);
-      
-      System.out.println(following.getEntity() + " / id=" + following.getId());
-      System.out.println(followingProfile.getBasic().getName());
-      printPosts(followingTentClient.getPosts());
-    }
-    
-    HashMap<String, String> scopes = new HashMap<String, String>();
-    scopes.put("write_profile", "Not really used, just testing");
+    TentClient tentClientSync = new TentClient();
+    tentClientSync.discover("https://mwanji.tent.is");
+    Profile profile = tentClientSync.getProfile();
+//    
+//    System.out.println(profile.getCore().getEntity());
+//    System.out.println(profile.getBasic().getName());
+//    System.out.println(profile.getBasic().getAvatarUrl());
+//    
+//    List<Post> posts = tentClient.getPosts();
+//    
+//    printPosts(posts);
+//    
+//    List<Following> followings = tentClient.getFollowings();
+//    
+//    for (Following following : followings) {
+//      TentClient followingTentClient = new TentClient();
+//      followingTentClient.discover(following.getEntity());
+//      Profile followingProfile = followingTentClient.getProfile();
+//      Following followingDetail = tentClient.getFollowing(following);
+//      
+//      System.out.println(following.getEntity() + " / id=" + following.getId());
+//      System.out.println(followingProfile.getBasic().getName());
+//      printPosts(followingTentClient.getPosts());
+//    }
+//    
+//    HashMap<String, String> scopes = new HashMap<String, String>();
+//    scopes.put("write_profile", "Not really used, just testing");
     
 //    RegistrationResponse registrationResponse = tentClient.register(new RegistrationRequest("Java Client Test", "Running dev tests", "http://www.moandjiezana.com/tent-client-java", new String [] { "http://www.moandjiezana.com/tent-client-java" }, scopes));
 //    
@@ -53,6 +63,22 @@ public class TentClientTest {
 //    System.out.println(registrationResponse.getMacKey());
 //    System.out.println(registrationResponse.getMacKeyId());
 //    System.out.println(registrationResponse.getMacAlgorithm());
+  }
+  
+  @Test
+  public void should_discover_profile_from_header() throws Exception {
+    server.addExpectation(onRequestTo("/").withMethod(Method.HEAD), giveEmptyResponse().withContentType("text/html;charset=utf-8").withHeader("Link", "<" + server.getBaseUrl() + "/tent/profile>; rel=\"https://tent.io/rels/profile\""));
+    server.addExpectation(onRequestTo("/tent/profile").withHeader("Accept", "application/vnd.tent.v0+json"), giveResponse(getProfileJson()).withContentType("application/vnd.tent.v0+json; charset=utf-8"));
+    
+    tentClient.discover(server.getBaseUrl()).get();
+    Profile profile = tentClient.getProfile().get();
+    
+    assertEquals(server.getBaseUrl(), profile.getCore().getEntity());
+    assertEquals("Mwanji Ezana", profile.getBasic().getName());
+  }
+  
+  private String getProfileJson() {
+    return "{\"https://tent.io/types/info/basic/v0.1.0\":{\"name\":\"Mwanji Ezana\",\"avatar_url\":\"http://www.gravatar.com/avatar/ae8715093d8d4219507146ed34f0ed16.png\",\"birthdate\":\"\",\"location\":\"\",\"gender\":\"M\",\"bio\":\"\",\"permissions\":{\"public\":true}},\"https://tent.io/types/info/core/v0.1.0\":{\"entity\":\"" + server.getBaseUrl() + "\",\"licenses\":[],\"servers\":[\"https://mwanji.tent.is/tent\"],\"permissions\":{\"public\":true}}}";
   }
 
   private void printPosts(List<Post> posts) {
